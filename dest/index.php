0521a5128ee0ecdb5136403bba13eb7ee78848fa
高砂恵実 <takasuna@ondo-inc.jp>
2025-03-18 15:43:03 +0900

    favicon関連のコード、ディレクトリの削除

--- dest/index.php
+++ dest/index.php
@@ -32,14 +32,6 @@
 
     <!--サイトアイコン-->
     <link rel="icon" href="/favicon.ico" type="image/vnd.microsoft.icon">
-    <!-- ICON-->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $documentRoot; ?>/assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $documentRoot; ?>/assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $documentRoot; ?>/assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo $documentRoot; ?>/assets/favicon/site.webmanifest" crossorigin="use-credentials">
-    <link rel="mask-icon" href="<?php echo $documentRoot; ?>/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
 
     <!--電話番号,メールアドレスのリンク変換防止-->
     <meta name="format-detection" content="email=no,telephone=no,address=no"/>
