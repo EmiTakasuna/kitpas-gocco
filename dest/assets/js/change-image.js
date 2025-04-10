bf587b7c688104c1bc0cc5de990e083c9459dcca
高砂恵実 <takasuna@ondo-inc.jp>
2025-04-10 11:46:46 +0900

    テキストの修正、ファーストビューアニメーションの修正

--- dest/assets/js/change-image.js
+++
@@ -1,7 +0,0 @@
-const changeimg = document.getElementById("img1");
-    changeimg.addEventListener( 'mouseenter', () => {
-        changeimg.src = "https://dubdesign.net/wp-content/uploads/2022/02/java_numberchange.jpg";
-    } );
-    changeimg.addEventListener( 'mouseleave', () => {
-        changeimg.src = "https://dubdesign.net/wp-content/uploads/2022/02/java_apitab.jpg";
-    } );
